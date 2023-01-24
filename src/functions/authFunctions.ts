03acda07e89acfb4f6645f3bb106ce2a70e4d95d
rpsloup <rpsloup@proton.me>
2023-01-24 10:58:49 +0100

    Loading auth from cache

--- src/functions/authFunctions.ts
+++ src/functions/authFunctions.ts
@@ -1,6 +1,9 @@
-import type { UserAuth } from '../typings/authTypes';
+import * as fs from 'fs';
 
 import { shell } from '../main';
+import { DATA_FOLDER } from '../main';
+
+import type { UserAuth } from '../typings/authTypes';
 
 export const getAuthFromInput = (): UserAuth => {
   console.log('Enter the URL of Bakaláři')
@@ -16,4 +19,25 @@ export const getAuthFromInput = (): UserAuth => {
     userName,
     password,
   };
+};
+
+export const getAuthFromCache = (): UserAuth | null => {
+  if (!fs.existsSync(`${DATA_FOLDER}/auth.json`)) return null;
+  const authData = fs.readFileSync(`${DATA_FOLDER}/auth.json`);
+  const auth = JSON.parse(authData.toString());
+  return auth;
+};
+
+const createAuthFolder = () => {
+  if (!fs.existsSync(DATA_FOLDER)) {
+    fs.mkdirSync(DATA_FOLDER);
+  }
+};
+
+export const saveAuth = (auth: UserAuth) => {
+  createAuthFolder();
+  fs.writeFileSync(
+    `${DATA_FOLDER}/auth.json`,
+    JSON.stringify(auth, null, 2)
+  );
 };
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
 import { Shell } from './shell';
 import { printBanner } from './functions/bannerFunctions';
-import { getAuthFromInput } from './functions/authFunctions';
+import {
+  getAuthFromCache,
+  getAuthFromInput
+} from './functions/authFunctions';
 import { fetchToken } from './functions/fetchFunctions';
+import { saveAuth } from './functions/authFunctions';
 
 export const shell = new Shell();
 export const HOSTNAME = 'bakalari';
 export const BANNER_FOLDER = 'banners';
+export const DATA_FOLDER = 'data';
 
 (async () => {
   shell.setHostname(HOSTNAME);
   printBanner('welcome');
-  const auth = getAuthFromInput();
+  const auth = getAuthFromCache() ?? getAuthFromInput();
   const tokenData = await fetchToken(auth);
 
   if (!tokenData) {
@@ -18,6 +23,7 @@ export const BANNER_FOLDER = 'banners';
     return;
   }
 
+  saveAuth(auth);
   shell.setUserName(auth.userName);
   shell.getCommand();
 })();
